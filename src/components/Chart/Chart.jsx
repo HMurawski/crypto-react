3a336c17493286d7ec61171b5153855af3caa3d2
Hubert Murawski <h.murawski9@gmail.com>
2023-09-26 14:06:21 +0200

    added charts and implemented all data+chart view

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,19 +1,75 @@
-import { useLayoutEffect, useState } from "react"
+import { useLayoutEffect, useState, useContext } from "react"
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, Legend, ResponsiveContainer,  } from 'recharts';
+import { GlobalState } from "../../context/context/Context";
+
+
+function CustomTooltip({ payload, label, active, pickCurrency = "pln" }) {
+    if (active && payload && payload.length>0) {
+      return (
+        <div className="custom-tooltip">
+          <p className="label text-sm">{`${label} : ${new Intl.NumberFormat("en-US", {
+												style: "currency",
+												currency: pickCurrency,
+                                                minimumFractionDigits: 3
+											}).format(payload[0].value)}`}</p>
+         
+        </div>
+      );
+    }
+  
+    return null;
+  }
+
+
+const ChartComp = ({data, currency, type}) => {
+    const tooltipLabelStyle = {
+        color: "blue",  
+    };
+
+
+    return (
+        <ResponsiveContainer height={"90%"}  >
+        <LineChart  data={data} style={{marginTop: '30px'}}>
+        <Line type="monotone" dataKey={type} stroke="#ffffff" strokeWidth={"2px"} />
+        <CartesianGrid stroke="#f6eaff" strokeWidth={"0.5px"} />
+        <XAxis dataKey="date" stroke="#ffffff" hide />
+        <YAxis  stroke="#ffffff" dataKey={type} hide domain={["auto", "auto"]} />
+        <Tooltip content={<CustomTooltip  currency={currency} />} cursor={false} />
+        <Legend />
+      </LineChart>
+      </ResponsiveContainer> 
+    )
+}
+ 
+
 
 const Chart = ({id}) => {
 
     const [chartData, setChartData] = useState()
+    let {pickCurrency} = useContext(GlobalState)
+    const [type, setType] = useState("prices")
+    const [days, setDays] = useState(7)
 
 useLayoutEffect(()=>{
 const getChartData = async (id) => {
     try {
         const data = await fetch(
-            `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=pln&days=7&interval=daily`
+            `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=pln&days=${days}&interval=daily`
         )
             .then((res) => res.json())
             .then((json) => json);
-            setChartData(data)
-            console.log(chartData);
+
+            let convertedData = data[type].map((item)=>{
+              {
+                return {
+                    date: new Date(item[0]).toLocaleDateString(),
+                    [type]: item[1]
+                }
+              }
+            })
+            console.log(convertedData);
+            setChartData(convertedData)
+           
     } catch (error) {
         console.log(error);
     }
@@ -21,13 +77,22 @@ const getChartData = async (id) => {
 }
 
 getChartData(id)
-}, [id])
+}, [id, type, days])
 
 
-    return (<div>
+    return (<div className=" w-full h-[80%]">
+
+        <ChartComp data={chartData} currency={pickCurrency} type={type}/>
+        <div className="flex ">
+            <button onClick={() => setType("prices")}>Prices</button>
+            <button onClick={() => setType("market_caps")}>Market Caps</button>
+            <button onClick={() => setType("total_volumes")}>Total Volumes</button>
 
 
-
+            <button onClick={() => setDays(7)}>7d</button>
+            <button onClick={() => setDays(14)}>14d</button>
+            <button onClick={() => setDays(30)}>30d</button>
+        </div>
 
     </div>)
 }
--- src/components/CryptoDetails/CryptoDetails.jsx
+++ src/components/CryptoDetails/CryptoDetails.jsx
@@ -4,27 +4,30 @@ import ReactDOM from "react-dom";
 import { useNavigate, useParams } from "react-router-dom";
 import Chart from "../Chart/Chart";
 
+const Indicator = ({ current_price, high, low }) => {
+	const [green, setGreen] = useState();
 
-const Indicator = ({current_price, high, low }) => {
+	useEffect(() => {
+		let total = high - low;
+		let greenZone = ((high - current_price) * 100) / total;
+		setGreen(Math.ceil(greenZone));
+	}, [current_price, high, low]);
 
-    const [green, setGreen] = useState()
-
-
-    useEffect(()=>{
-        let total = high - low
-        let greenZone = ((high - current_price) * 100) / total
-        setGreen(Math.ceil(greenZone))
-    }, [current_price, high, low ])
-
-
-    return (<>
-      <span className="bg-green h-1.5 rounded-r-lg w-[50%] " style={{width: `${green}%`}}>&nbsp;</span>
-        <span className="bg-red h-1.5 rounded-l-lg w-[50%] " style={{width: `${100 - green}%`}}>&nbsp;</span>
-      
-
-
-    </>)
-}
+	return (
+		<>
+			<span
+				className="bg-green h-1.5 rounded-r-lg w-[50%] "
+				style={{ width: `${green}%` }}>
+				&nbsp;
+			</span>
+			<span
+				className="bg-red h-1.5 rounded-l-lg w-[50%] "
+				style={{ width: `${100 - green}%` }}>
+				&nbsp;
+			</span>
+		</>
+	);
+};
 
 const CryptoDetails = (props) => {
 	let { coinId } = useParams();
@@ -44,21 +47,21 @@ const CryptoDetails = (props) => {
 			className=" fixed top-0 w-full h-full bg-orchid bg-opacity-30 backdrop-blur-sm flex items-center justify-center "
 			onClick={closePopup}>
 			<div
-				className="w-[60%] h-[65%] bg-blue opacity-75 rounded-lg text-white relative "
+				className="w-[60%] h-[60%] bg-darkblue opacity-75 rounded-lg text-white relative "
 				onClick={(e) => {
 					e.stopPropagation();
 				}}>
 				{coinData ? (
 					<div className="flex items-center  justify-around h-full w-full p-4 ">
 						<div className=" flex flex-col w-[45%] h-full pr-2">
-							<div className="flex w-full items-center">
+							<div className="flex w-full items-center my-2">
 								<img
-									className="w-[3rem] h-[3rem] mx-1.5"
+									className="w-[3rem] h-[3rem] mx-2"
 									src={coinData.image.large}
 									alt={coinData.id}
 								/>
 								<h2 className="text-xl font-bold ">{coinData.name}</h2>
-								<span className="text-sm py-1 px-2.5 ml-2 rounded uppercase bg-purple bg-opacity-20">
+								<span className="text-base py-1 px-2.5 ml-2 rounded uppercase bg-purple bg-opacity-20">
 									{coinData.symbol}
 								</span>
 							</div>
@@ -66,9 +69,9 @@ const CryptoDetails = (props) => {
 							<div className="flex w-full mt-6 ">
 								<div className="flex flex-col w-full ">
 									<div className="flex justify-between ">
-										<span className="text-sm">Price</span>
+										<span className="text-base">Price</span>
 										<div
-											className={`text-sm px-1 ml-2 font-medium flex items-center rounded uppercase 
+											className={`text-base px-1 ml-2 font-medium flex items-center rounded uppercase 
                                         
                                         ${
 																					coinData.market_data
@@ -79,7 +82,7 @@ const CryptoDetails = (props) => {
                                         
                                         
                                         `}>
-											<span className="text-sm mx-1">
+											<span className="text-base mx-1">
 												{Number(
 													coinData.market_data.price_change_percentage_24h
 												).toFixed(2)}
@@ -104,7 +107,7 @@ const CryptoDetails = (props) => {
 
 							<div className="flex w-full mt-4 justify-between ">
 								<div className="flex flex-col">
-									<span className="text-sm">Market Cap</span>
+									<span className="text-base">Market Cap</span>
 									<h3 className="text-base font-bold">
 										{new Intl.NumberFormat("en-US", {
 											style: "currency",
@@ -114,7 +117,7 @@ const CryptoDetails = (props) => {
 									</h3>
 								</div>
 								<div className="flex flex-col">
-									<span className="text-sm">Fully Diluted Valuation</span>
+									<span className="text-base">Fully Diluted Valuation</span>
 									<h3 className="text-base font-bold">
 										{new Intl.NumberFormat("en-US", {
 											style: "currency",
@@ -128,7 +131,7 @@ const CryptoDetails = (props) => {
 							</div>
 
 							<div className="flex flex-col w-full mt-4 justify-between ">
-								<span className="text-sm">Total Volume</span>
+								<span className="text-base">Total Volume</span>
 								<h3 className="text-base font-bold">
 									{new Intl.NumberFormat("en-US", {
 										style: "currency",
@@ -138,15 +141,19 @@ const CryptoDetails = (props) => {
 								</h3>
 							</div>
 
-							<div className="flex w-full mt-4 justify-between"> 
-                            <Indicator current_price={coinData.market_data.current_price[pickCurrency]} 
-                            high={coinData.market_data.high_24h[pickCurrency]}
-                            low={coinData.market_data.low_24h[pickCurrency]} />
-                            </div>
+							<div className="flex w-full mt-4 justify-between">
+								<Indicator
+									current_price={
+										coinData.market_data.current_price[pickCurrency]
+									}
+									high={coinData.market_data.high_24h[pickCurrency]}
+									low={coinData.market_data.low_24h[pickCurrency]}
+								/>
+							</div>
 
 							<div className="flex w-full mt-4 justify-between ">
 								<div className="flex flex-col">
-									<span className="text-sm">High 24h</span>
+									<span className="text-base">High 24h</span>
 									<h3 className="text-base font-bold text-green ">
 										{new Intl.NumberFormat("en-US", {
 											style: "currency",
@@ -156,7 +163,7 @@ const CryptoDetails = (props) => {
 									</h3>
 								</div>
 								<div className="flex flex-col">
-									<span className="text-sm">Low 24h</span>
+									<span className="text-base">Low 24h</span>
 									<h3 className="text-base font-bold text-red">
 										{new Intl.NumberFormat("en-US", {
 											style: "currency",
@@ -169,47 +176,61 @@ const CryptoDetails = (props) => {
 
 							<div className="flex w-full mt-4 justify-between">
 								<div className="flex flex-col">
-									<a target="_blank" rel="noreferrer" className="text-sm px-1.5 py-0.5 my-1 rounded" href={coinData?.links?.homepage[0]}>{coinData?.links?.homepage[0].substring(0,30)}</a>
-									<a target="_blank" rel="noreferrer" className="text-sm px-1.5 py-0.5 my-1 rounded" href={coinData?.links?.blockchain_site[0]}>{coinData?.links?.blockchain_site[0].substring(0,30)}</a>
+									<a
+										target="_blank"
+										rel="noreferrer"
+										className="text-base px-1.5 py-0.5 my-1 rounded"
+										href={coinData?.links?.homepage[0]}>
+										{coinData?.links?.homepage[0].substring(0, 30)}
+									</a>
+									<a
+										target="_blank"
+										rel="noreferrer"
+										className="text-base px-1.5 py-0.5 my-1 rounded"
+										href={coinData?.links?.blockchain_site[0]}>
+										{coinData?.links?.blockchain_site[0].substring(0, 30)}
+									</a>
 
-                                        {
-                                            coinData?.links?.official_forum_url[0] && <a target="_blank" rel="noreferrer" className="text-sm px-1.5 py-0.5 my-1 rounded" href={coinData?.links?.official_forum_url[0]}>{coinData?.links?.official_forum_url[0].substring(0,30)}</a>
-                                        }
-
-									
+									{coinData?.links?.official_forum_url[0] && (
+										<a
+											target="_blank"
+											rel="noreferrer"
+											className="text-base px-1.5 py-0.5 my-1 rounded"
+											href={coinData?.links?.official_forum_url[0]}>
+											{coinData?.links?.official_forum_url[0].substring(0, 30)}
+										</a>
+									)}
 								</div>
 
 								<div className="flex flex-col content-start">
-									<span className="text-sm capitalize">sentiment</span>
+									<span className="text-base capitalize">sentiment</span>
 									<div className="flex justify-between ">
 										<div
-											className={`text-sm px-1 ml-2 my-1 font-medium flex items-center rounded uppercase text-green bg-green bg-opacity-25`}>
-											<span className="text-sm mx-1">
+											className={`text-base px-1 ml-2 my-1 font-medium flex items-center rounded uppercase text-green bg-green bg-opacity-25`}>
+											<span className="text-base mx-1">
 												{Number(coinData.sentiment_votes_up_percentage).toFixed(
 													2
 												)}
 												%
 											</span>
-										
 										</div>
 									</div>
 									<div className="flex justify-between ">
 										<div
-											className={`text-sm px-1 ml-2  my-1 font-medium flex items-center rounded uppercase text-red bg-red bg-opacity-25`}>
-											<span className="text-sm mx-1">
+											className={`text-base px-1 ml-2  my-1 font-medium flex items-center rounded uppercase text-red bg-red bg-opacity-25`}>
+											<span className="text-base mx-1">
 												{Number(
 													coinData.sentiment_votes_down_percentage
 												).toFixed(2)}
 												%
 											</span>
-											
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 
-						<div className="flex flex-col w-[45%] h-full pl-4 bg-green">
+						<div className="flex flex-col w-[45%] h-full pl-4 ">
 							<Chart id={coinData.id} />
 						</div>
 					</div>
